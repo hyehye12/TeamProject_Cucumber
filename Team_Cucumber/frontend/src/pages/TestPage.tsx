8583b3316acaf27a5a71681d81e6bffdf037a4c5
vello101 <vello101@naver.com>
2025-10-31 19:02:15 +0900

    chore: 버튼 컴포넌트 기초 뼈대, 이름 수정

--- Team_Cucumber/frontend/src/pages/TestPage.tsx
+++ Team_Cucumber/frontend/src/pages/TestPage.tsx
@@ -8,7 +8,7 @@ export default function TestPage() {
         <NavLink to="/test">Test</NavLink>
         <NavLink to="Hemin">Hemin</NavLink>
         <NavLink to="Nara">Nara</NavLink>
-        <NavLink to="Juwone">Juwone</NavLink>
+        <NavLink to="Joowon">Joowon</NavLink>
       </nav>
       <hr />
       <Outlet /> {/* 자식 라우트가 여기로 렌더링 */}
@@ -23,12 +23,12 @@ export function TestHome() {
 
 // 같은 파일 안에 PersonPage 정의
 export function PersonPage() {
-  const { name } = useParams<{ name: "Hemin" | "Nara" | "Juwone" }>();
+  const { name } = useParams<{ name: "Hemin" | "Nara" | "Joowon" }>();
 
   const PEOPLE = {
     Hemin: { title: "혜민" },
     Nara: { title: "나라" },
-    Juwone: { title: "주원" },
+    Joowon: { title: "주원" },
   } as const;
 
   const person = name ? PEOPLE[name] : undefined;
