93d9f168cb6709257ca6af4300567421460db5e7
vello101 <vello101@naver.com>
2025-11-05 20:07:03 +0900

    feat: Button 컴포넌트 생성

--- Team_Cucumber/frontend/src/components/common/Button.tsx
+++ Team_Cucumber/frontend/src/components/common/Button.tsx
@@ -1,5 +1,18 @@
-const Button = () => {
-  return <div></div>;
+import type { ButtonHTMLAttributes } from "react";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({ children, className: _className, ...props }: ButtonProps) => {
+  const className = [
+    "bg-orange-500 text-white py-3 px-20 rounded-md font-bold cursor-pointer hover:bg-orange-300 disabled:bg-gray-300",
+    _className,
+  ].join(" ");
+
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- Team_Cucumber/frontend/src/pages/test/TestJoowonPage.tsx
+++ Team_Cucumber/frontend/src/pages/test/TestJoowonPage.tsx
@@ -1,3 +1,11 @@
+import Button from "../../components/common/Button";
+
 export const TestJoowonPage = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-center">
+      <Button className="w-1/2" disabled>
+        확인
+      </Button>
+    </div>
+  );
 };
