2f12ba84b4f678051b3e762911a65145dbd1258c
narahub123 <parknara0213@gmail.com>
2025-11-08 11:58:53 +0900

    Fix: TestNaraPage 수정

--- Team_Cucumber/frontend/src/pages/test/TestNaraPage.tsx
+++ Team_Cucumber/frontend/src/pages/test/TestNaraPage.tsx
@@ -24,6 +24,10 @@ export const TestNaraPage = () => {
   return (
     <div>
       <Toast />
+      <div className="flex gap-2">
+        <Checkbox id="test" checked={isChecked} onChange={handleChange} />
+        <label htmlFor="test">테스트</label>
+      </div>
     </div>
   );
 };
