47baa4162db74feb7750d04a7410c4de12333ea4
vello101 <vello101@naver.com>
2025-11-16 00:08:22 +0900

    feat: ccp를 이용한 모달 컴포넌트

---
+++ Team_Cucumber/frontend/src/components/common/Modal/Modal.types.ts
@@ -0,0 +1,23 @@
+import { type ReactNode } from "react";
+
+export interface ModalRootProps {
+  open: boolean;
+  children: ReactNode;
+}
+
+export interface HeaderProps {
+  title: string;
+}
+
+export interface BodyProps {
+  children: ReactNode;
+}
+
+export interface FooterProps {
+  children: ReactNode;
+}
+
+export interface ModalButtonProps {
+  children: ReactNode;
+  onClick: () => void;
+}
--- Team_Cucumber/frontend/src/pages/test/TestJoowonPage.tsx
+++ Team_Cucumber/frontend/src/pages/test/TestJoowonPage.tsx
@@ -1,35 +1,38 @@
 import Button from "../../components/common/Button/Button";
-import ConfirmModal from "../../components/common/Modal/ConfirmModal";
+import Modal from "../../components/common/Modal/Modal";
 import NeighborhoodAuthModal from "../../components/common/Modal/NeighborhoodAuthModal";
 import { useState } from "react";
 
 export const TestJoowonPage = ({}) => {
   const [showModal, setShowModal] = useState(false);
-  const regionName = "일원본";
 
   const handleConfirm = () => {
-    console.log("확인 버튼");
-  };
-
-  // 버튼 컴포넌트(확인버튼) 클릭 시 모달 열림
-  const handleModalOpen = () => {
-    setShowModal(true);
+    setShowModal(false);
   };
 
   return (
     <div>
-      <Button className="w-1/2" onClick={handleModalOpen}>
+      <Button className="w-1/2" onClick={() => setShowModal(true)}>
         확인
       </Button>
-
-      {/* 모달 닫기 */}
-      {showModal && (
-        <NeighborhoodAuthModal
-          onClose={() => setShowModal(false)}
-          region={regionName}
-          onConfirm={handleConfirm}
-        />
-      )}
+      {/* <Modal open={showModal}>
+        <Modal.Header title="모달 title" />
+        <Modal.Body>모달 body</Modal.Body>
+        <Modal.Footer>
+          <Modal.CancelButton onClick={() => setShowModal(false)}>
+            취소
+          </Modal.CancelButton>
+          <Modal.ConfirmButton onClick={handleConfirm}>
+            확인
+          </Modal.ConfirmButton>
+        </Modal.Footer>
+      </Modal> */}
+      <NeighborhoodAuthModal
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        region="역삼"
+        onConfirm={handleConfirm}
+      />
     </div>
   );
 };
