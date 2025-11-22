3a5a069dd0147e82769279ab7804395c2f85b7be
narahub123 <parknara0213@gmail.com>
2025-11-22 13:20:49 +0900

    Feat: ReportsUnusableProduct 생성

--- Team_Cucumber/frontend/src/components/reports/mains/ReportsUnusableProduct.tsx
+++ Team_Cucumber/frontend/src/components/reports/mains/ReportsUnusableProduct.tsx
@@ -5,9 +5,13 @@ const ReportsUnusableProduct = () => {
   return (
     <ReportsLayout>
       <ReportsLayout.Header>
-        <ReportsMainTitle title="" />
+        <ReportsMainTitle title="사용할 수 없는 상품" />
       </ReportsLayout.Header>
       <ReportsLayout.Main>
+        <p>
+          상품 상태가 매우 좋지 않거나 연식이 오래되어서 더 이상 사용할 수 없는
+          상품을 신고해주세요.
+        </p>
         <BlockUserChecker />
       </ReportsLayout.Main>
       <ReportsLayout.Footer>
