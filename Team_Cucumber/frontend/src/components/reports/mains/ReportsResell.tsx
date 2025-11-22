906d65752bd48d8924e68ff01464c01704fa1563
narahub123 <parknara0213@gmail.com>
2025-11-22 13:25:51 +0900

    Feat: ReportsResell 생성

--- Team_Cucumber/frontend/src/components/reports/mains/ReportsResell.tsx
+++ Team_Cucumber/frontend/src/components/reports/mains/ReportsResell.tsx
@@ -1,17 +1,50 @@
 import { ReportsLayout } from "../../../layout";
-import { BlockUserChecker, ReportsButton, ReportsMainTitle } from "../..";
+import {
+  BlockUserChecker,
+  ReportsButton,
+  ReportsMainTitle,
+  Textarea,
+  useReportsContext,
+} from "../..";
+import { useEffect, type ChangeEvent } from "react";
 
 const ReportsResell = () => {
+  const { handleReport, reportText, setReportText } = useReportsContext();
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const text = e.target.value;
+
+    setReportText({ type: "fraud", text });
+  };
+
+  useEffect(() => {
+    setReportText({ type: "fraud", text: "" });
+  }, []);
+
   return (
     <ReportsLayout>
       <ReportsLayout.Header>
-        <ReportsMainTitle title="" />
+        <ReportsMainTitle title="나에게 구매 후 비씨게 재판매" />
       </ReportsLayout.Header>
       <ReportsLayout.Main>
+        <p>
+          어떠한 경우에도 당근에서 구매하거나 나눔 받은 물건을 더 높은 가겨으로
+          재판매할 수 없습니다. 반복적으로 재판매하는 사용자를 발견할 경우
+          알려주세요.
+        </p>
+        <div>
+          <Textarea
+            placeholder="신고내용을 입력해주세요.(최대 300자)"
+            className="border border-gray-200 mb-0"
+            value={reportText?.text}
+            onChange={handleChange}
+          />
+          <p className="text-right text-gray-400">{`${reportText?.text.length}/300`}</p>
+        </div>
         <BlockUserChecker />
       </ReportsLayout.Main>
       <ReportsLayout.Footer>
-        <ReportsButton />
+        <ReportsButton onClick={handleReport} />
       </ReportsLayout.Footer>
     </ReportsLayout>
   );
