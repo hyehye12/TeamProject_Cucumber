ae5c674a4ec20f4f2ab613326d8b71e7c67eaf89
narahub123 <parknara0213@gmail.com>
2025-11-22 13:22:49 +0900

    Feat: ReportsDuplicatePost 생성

--- Team_Cucumber/frontend/src/components/reports/mains/ReportsDuplicatePost.tsx
+++ Team_Cucumber/frontend/src/components/reports/mains/ReportsDuplicatePost.tsx
@@ -1,14 +1,18 @@
 import { ReportsLayout } from "../../../layout";
-import { BlockUserChecker, ReportsButton, ReportsMainTitle } from "../..";
+import { ReportsButton, ReportsMainTitle } from "../..";
 
 const ReportsDuplicatePost = () => {
   return (
     <ReportsLayout>
       <ReportsLayout.Header>
-        <ReportsMainTitle title="" />
+        <ReportsMainTitle title="중복 게시글" />
       </ReportsLayout.Header>
       <ReportsLayout.Main>
-        <BlockUserChecker />
+        <p>
+          당근은 최대 2개의 지역에서 사용할 수 있고 서로 다른 동네에서는 똑같은
+          게시글을 작성할 수 있어요. 신고하고자 하는 게시글이 같은 동네에서
+          작성된 경우에만 신고해주세요.
+        </p>
       </ReportsLayout.Main>
       <ReportsLayout.Footer>
         <ReportsButton />
