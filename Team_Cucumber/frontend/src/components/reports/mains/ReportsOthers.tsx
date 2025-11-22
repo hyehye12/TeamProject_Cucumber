333b5ab2bd4751edb28948378aaa07c3bb480457
narahub123 <parknara0213@gmail.com>
2025-11-22 12:29:34 +0900

    Feat: ReportsOthers 생성

--- Team_Cucumber/frontend/src/components/reports/mains/ReportsOthers.tsx
+++ Team_Cucumber/frontend/src/components/reports/mains/ReportsOthers.tsx
@@ -1,17 +1,41 @@
 import { ReportsLayout } from "../../../layout";
-import { BlockUserChecker, ReportsButton, ReportsMainTitle } from "../..";
+import {
+  ReportsButton,
+  ReportsMainTitle,
+  Textarea,
+  useReportsContext,
+} from "../..";
+import { useEffect, type ChangeEvent } from "react";
 
 const ReportsOthers = () => {
+  const { handleReport, reportText, setReportText } = useReportsContext();
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const text = e.target.value;
+
+    setReportText({ type: "bans", text });
+  };
+
+  useEffect(() => {
+    setReportText({ type: "bans", text: "" });
+  }, []);
+
   return (
     <ReportsLayout>
       <ReportsLayout.Header>
-        <ReportsMainTitle title="저작권 침해 물품" />
+        <ReportsMainTitle title="기타" />
       </ReportsLayout.Header>
       <ReportsLayout.Main>
-        <BlockUserChecker />
+        <Textarea
+          placeholder="신고내용을 입력해주세요.(최대 300자)"
+          className="border border-gray-200 mb-0"
+          value={reportText?.text}
+          onChange={handleChange}
+        />
+        <p className="text-right text-gray-400">{`${reportText?.text.length}/300`}</p>
       </ReportsLayout.Main>
       <ReportsLayout.Footer>
-        <ReportsButton />
+        <ReportsButton onClick={handleReport} />
       </ReportsLayout.Footer>
     </ReportsLayout>
   );
