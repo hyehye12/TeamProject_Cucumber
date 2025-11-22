32e4249c5130cf8f17f1299e83766e058b483e33
narahub123 <parknara0213@gmail.com>
2025-11-22 12:13:29 +0900

    Feat: ReportsProhibitedCosmetics 생성

--- Team_Cucumber/frontend/src/components/reports/mains/ReportsProhibitedCosmetics.tsx
+++ Team_Cucumber/frontend/src/components/reports/mains/ReportsProhibitedCosmetics.tsx
@@ -5,9 +5,13 @@ const ReportsProhibitedCosmetics = () => {
   return (
     <ReportsLayout>
       <ReportsLayout.Header>
-        <ReportsMainTitle title="저작권 침해 물품" />
+        <ReportsMainTitle title="거래 금지 화장품" />
       </ReportsLayout.Header>
       <ReportsLayout.Main>
+        <p>
+          증정용 화장품 샘플은 개인 간 거래할 수 없어요. 다만 호텔 어메니티느
+          증정용 화장품 샘플에 해당하지 않아요. 이 점을 참고하여 신고해주세요.
+        </p>
         <BlockUserChecker />
       </ReportsLayout.Main>
       <ReportsLayout.Footer>
